25689f3df905839f6e634afc122fd6507a9bb37e
tishin-endou <t.endo@bestaff.co.jp>
2024-03-27 09:29:22 +0900

    add trace id

--- app/services/current-user.ts
+++ app/services/current-user.ts
@@ -166,6 +166,9 @@ export default class CurrentUserService extends Service {
         if (csrfToken) {
             headers['X-CSRFToken'] = csrfToken;
         }
+        if (window.traceId) {
+            headers['X-Amzn-Trace-Id'] = 'Root=' + window.traceId;
+        }
         return headers;
     }
 
