216b2b7c4f9b46cd6a577c51232d9c009bb55cc7
tishin-endou <t.endo@bestaff.co.jp>
2024-09-06 12:15:41 +0900

    query definition

--- app/dashboard/controller.ts
+++ app/dashboard/controller.ts
@@ -88,10 +88,10 @@ export default class Dashboard extends Controller {
 
         const nodes: QueryHasManyResult<Node> = yield user.queryHasMany('sparseNodes', {
             embed: ['parent', 'root'],
-             fields: {
-                 users: 'full_name, given_name, middle_names, family_name',
-                 'sparse-nodes': 'id,title, date_modified, bibliographic_contributors, parent, root, creator',
-             },
+            fields: {
+                users: 'full_name, given_name, middle_names, family_name',
+                'sparse-nodes': 'id,title, date_modified, bibliographic_contributors, parent, root, creator',
+            },
             // eslint-disable-next-line ember/no-global-jquery
             filter: this.filter ? { title: $('<div>').text(this.filter).html() } : undefined,
             page: more ? this.incrementProperty('page') : this.set('page', 1),
